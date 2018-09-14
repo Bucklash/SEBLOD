49e4a7e5569567264d1ba4f929291cb35c8ca745
Jon Buckner <jon.buckner@sky.com>
2018-09-15 06:43:50 +0800

    Updated with tcpdf some param
    Taken from Example 001
    https://tcpdf.org/examples/example_001/

--- plugins/cck_field/jb_pdf/tmpl/edit.php
+++ plugins/cck_field/jb_pdf/tmpl/edit.php
@@ -26,10 +26,9 @@ $to_admin   =   ( is_array( @$options2['to_admin'] ) ) ? implode( ',', $options2
         <?php
         
         /*
-        * jb_pdf_create 
-        * jb_pdf_create_field
+        * jb_pdf_create, jb_pdf_create_field
         * copied from 
-        * core_options_send and send_field
+        * core_options_send, send_field
         * options: Never, Always, Add, Edit
         * jb_pdf_create_field will reference a user created field to override "Never" i.e. with select field as yes=1 no=0]
         */
@@ -42,35 +41,94 @@ $to_admin   =   ( is_array( @$options2['to_admin'] ) ) ? implode( ',', $options2
                         <option value="2">Edit</option>
                     </optgroup>
                 </select>';
-        echo '<input type="text" id="json_options2_create_field" name="json[options2][create_field]" value="" class="inputbox text" size="14" maxlength="255">';
+        echo '<input type="text" id="json_options2_create_field" name="json[options2][create_field]" value="" class="inputbox text" placeholder="some_field_to_override" size="14" maxlength="255">';
 
- /*
-        * Paramaters for PDF
-        */       
-        
-        echo JCckDev::renderForm( 'core_options_from', @$options2['from_name'], $config, array( 'label'=>'From Name', 'selectlabel'=>'Use Global', 'options'=>'Name=1||Field=3', 'storage_field'=>'json[options2][from_name]' ) );
-        echo JCckDev::renderForm( 'core_options_from', @$options2['from'], $config );
-        echo JCckDev::renderForm( 'core_options_from_param', @$options2['from_param'], $config );
-        echo JCckDev::renderForm( 'core_options_from', @$options2['from_name'], $config, array( 'label'=>'From Name', 'selectlabel'=>'Use Global', 'options'=>'Name=1||Field=3', 'storage_field'=>'json[options2][from_name]' ) );
-        echo JCckDev::renderForm( 'core_options_from_param', @$options2['from_name_param'], $config, array( 'label'=>'From Name Field', 'storage_field'=>'json[options2][from_name_param]' ) );
-        echo JCckDev::renderForm( 'core_options_from', @$options2['reply_to'], $config, array( 'label'=>'Reply To', 'selectlabel'=>'', 'options'=>'None=0||Field=3', 'storage_field'=>'json[options2][reply_to]' ) );
-        echo JCckDev::renderForm( 'core_options_from_param', @$options2['reply_to_param'], $config, array( 'label'=>'Reply To Field', 'storage_field'=>'json[options2][reply_to_param]' ) );
-        echo JCckDev::renderForm( 'core_options_from', @$options2['reply_to_name'], $config, array( 'label'=>'Reply To Name', 'selectlabel'=>'', 'options'=>'None=0||Field=3', 'storage_field'=>'json[options2][reply_to_name]' ) );
-        echo JCckDev::renderForm( 'core_options_from_param', @$options2['reply_to_name_param'], $config, array( 'label'=>'Reply To Name Field', 'storage_field'=>'json[options2][reply_to_name_param]' ) );
-        echo JCckDev::renderForm( 'core_options_subject', @$options2['subject'], $config );
-        echo JCckDev::renderForm( 'core_options_to', @$options2['to'], $config );
-        echo JCckDev::renderForm( 'core_options_message', @$options2['message'], $config );
-        echo JCckDev::renderForm( 'core_options_to_field', @$options2['to_field'], $config );
-        echo JCckDev::renderForm( 'core_options_from_param', @$options2['message_field'], $config, array( 'label'=>'Message Field', 'defaultvalue'=>'', 'storage_field'=>'json[options2][message_field]' ) );
-        echo JCckDev::renderForm( 'core_options_to_admin', $to_admin, $config );
-        echo JCckDev::renderForm( 'core_options_from_param', @$options2['send_attachment_field'], $config, array( 'label'=>'Send Attachment Field', 'defaultvalue'=>'', 'storage_field'=>'json[options2][send_attachment_field]' ) );
-        echo JCckDev::renderForm( 'core_options_to', @$options2['attachment_field'], $config, array( 'label'=>'Attachment Field', 'defaultvalue'=>'', 'storage_field'=>'json[options2][attachment_field]' ) );
-        echo JCckDev::renderForm( 'core_options_from', @$options2['cc'], $config, array( 'label'=>'CC', 'selectlabel'=>'', 'options'=>'None=0||Email=1||Field=3', 'storage_field'=>'json[options2][cc]' ) );
-        echo JCckDev::renderForm( 'core_options_to', @$options2['cc_param'], $config, array( 'label'=>'CC Email Field', 'storage_field'=>'json[options2][cc_param]' ) );
-        echo JCckDev::renderForm( 'core_options_from', @$options2['bcc'], $config, array( 'label'=>'BCC', 'selectlabel'=>'', 'options'=>'None=0||Email=1||Field=3', 'storage_field'=>'json[options2][bcc]' ) );
-        echo JCckDev::renderForm( 'core_options_to', @$options2['bcc_param'], $config, array( 'label'=>'BCC Email Field', 'storage_field'=>'json[options2][bcc_param]' ) );
-        echo JCckDev::renderForm( 'core_size', $this->item->size, $config );
-        echo JCckDev::renderForm( 'core_dev_select', @$options2['format'], $config, array( 'label'=>'Format', 'defaultvalue'=>'1', 'selectlabel'=>'', 'options'=>'HTML=1||HTML as Plain Text=2||Plain Text=0', 'storage_field'=>'json[options2][format]' ) );
+        // ** TODO **
+        // Decide on which paramaters use
+        // How to manage multiple pages
+
+        /*
+        * Some of the many Paramaters for TCPDF
+        *
+        *
+        * // set document information
+        * $pdf->SetCreator(PDF_CREATOR);
+        * $pdf->SetAuthor('Nicola Asuni');
+        * $pdf->SetTitle('TCPDF Example 001');
+        * $pdf->SetSubject('TCPDF Tutorial');
+        * $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+        * 
+        * // set default header data
+        * $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
+        * $pdf->setFooterData(array(0,64,0), array(0,64,128));
+        * 
+        * // set header and footer fonts
+        * $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+        * $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+        * 
+        * // set default monospaced font
+        * $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+        * 
+        * // set margins
+        * $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        * $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+        * $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+        * 
+        * // set auto page breaks
+        * $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+        * 
+        * // set image scale factor
+        * $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+        * 
+        * // set some language-dependent strings (optional)
+        * if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+        *     require_once(dirname(__FILE__).'/lang/eng.php');
+        *     $pdf->setLanguageArray($l);
+        * }
+        * 
+        * // ---------------------------------------------------------
+        * 
+        * // set default font subsetting mode
+        * $pdf->setFontSubsetting(true);
+        * 
+        * // Set font
+        * // dejavusans is a UTF-8 Unicode font, if you only need to
+        * // print standard ASCII chars, you can use core fonts like
+        * // helvetica or times to reduce file size.
+        * $pdf->SetFont('dejavusans', '', 14, '', true);
+        * 
+        * // Add a page
+        * // This method has several options, check the source code documentation for more information.
+        * $pdf->AddPage();
+        * 
+        * // set text shadow effect
+        * $pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
+        * 
+        * // Set some content to print
+        * $html = <<<EOD
+        * <h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
+        * <i>This is the first example of TCPDF library.</i>
+        * <p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
+        * <p>Please check the source code documentation and other examples for further information.</p>
+        * <p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
+        * EOD;
+        * 
+        * // Print text using writeHTMLCell()
+        * $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+        * 
+        * // ---------------------------------------------------------
+        * 
+        * // Close and output PDF document
+        * // This method has several options, check the source code documentation for more information.
+        * $pdf->Output('example_001.pdf', 'I');
+        * 
+        * //============================================================+
+        * // END OF FILE
+        * //============================================================+
+        * 
+        * 
+        */
+
         /*
         * Add link to tutoril on forum
         */ 
@@ -83,11 +141,7 @@ $to_admin   =   ( is_array( @$options2['to_admin'] ) ) ? implode( ',', $options2
 
 <script type="text/javascript">
 jQuery(document).ready(function($) {
-    $('#json_options2_from_param').isVisibleWhen('json_options2_from','1,3',true,'visibility');
-    $('#json_options2_from_name_param').isVisibleWhen('json_options2_from_name','1,3',true,'visibility');
-    $('#json_options2_reply_to_param').isVisibleWhen('json_options2_reply_to','3',true,'visibility');
-    $('#json_options2_reply_to_name_param').isVisibleWhen('json_options2_reply_to_name','3',true,'visibility');
-    $('#json_options2_cc_param').isVisibleWhen('json_options2_cc','1,3',true,'visibility');
-    $('#json_options2_bcc_param').isVisibleWhen('json_options2_bcc','1,3',true,'visibility');
+    // If Never is selected, then show override field
+    $('#json_options2_create_field').isVisibleWhen('json_options2_create','0',true,'visibility');
 });
 </script>
